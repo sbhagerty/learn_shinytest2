9ce0f25dcd2f78b6e026950fe0a26491248347e6
sbhagerty <sbhagerty@gmail.com>
2022-05-25 15:15:47 +0000

    adding tests and githubactions

--- app.R
+++ app.R
@@ -1,3 +1,43 @@
+# https://jymaze.shinyapps.io/open_fda_a
+
+library(shiny)
+
+ui<- fluidPage(
+  titlePanel("Adverse Events Query to OpenFDA", windowTitle = "Adverse Events Query to OpenFDA"),
+  fluidRow(
+    column(3,
+           br(),
+           p("The OpenFDA project makes adverse-event data gathered after January 1, 2004,
+              available via a public-access portal that enables developers to quickly and
+              easily use it in applications. The project is hosted at:"),
+           a(href="https://open.fda.gov/", "open.fda.gov"),
+           br(),
+           br(),
+           p("This web-app provides a user-friendly graphical interface to the openFDA web-server"),
+           br(),
+           p("To explore the adverse event data between two dates:"),
+           textInput("api", "1) Enter the name of drug:", "asdsdsd"),
+           dateInput('start_date', '2) Enter the start date:', min = '2004-01-01', value = '2004-01-01'),
+           dateInput('end_date', '3) Enter the end date:', min = '2004-01-02'),
+           radioButtons('seriousness', '4) Filter by seriousness:', choices = c("All" = 0, "Serious" = 1, "Non-serious" = 2)),
+           actionButton("submit", "Submit Request"),
+           br(),
+           strong(htmlOutput("message")),
+           uiOutput("download_button"),
+           br(),
+           br()
+    ),
+    column(9,
+           plotOutput("plt", width = "auto", height = "640")
+    )
+  ),
+  fluidRow(
+    column(12,
+           DT::dataTableOutput("tble")
+    )
+  )
+)
+
 library(shiny)
 library(jsonlite)
 library(ggplot2)
@@ -38,7 +78,7 @@ subset_data <- function(df) {
 
 
 
-function(input, output) {
+server<- function(input, output) {
   
   api_data <- reactiveVal(NULL)
   
@@ -108,7 +148,7 @@ function(input, output) {
   
   output$message <- renderText(result_message())
   
-  output$tble <- renderDataTable({
+  output$tble <- DT::renderDataTable({
     req(api_data())
     api_data()
   })
@@ -134,5 +174,11 @@ function(input, output) {
       write.csv(api_data(), file, row.names = FALSE)
     }
   )
-  
+  exportTestValues(
+    api_data= api_data()
+    # plot = adverse_event_plot(df_subset(), display_drug())
+  )
 }
+
+
+shinyApp(ui, server)
---
+++ githubactions.R
@@ -0,0 +1,4 @@
+#basic github 
+usethis::use_github_action(url = "https://raw.githubusercontent.com/rstudio/actions/main/examples/connect-publish.yaml")
+
+usethis::use_github_action(url="https://raw.githubusercontent.com/rstudio/shinytest2/main/.github/workflows/test-actions.yaml")
